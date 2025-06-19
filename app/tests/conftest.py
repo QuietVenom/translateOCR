955a26302972c93c6cbb7bd921f7ed8b3751c17b
izco.marcos <izco.marcos@gmail.com>
2025-06-18 19:36:28 -0600

    mejoras de codigo 4.0

---
+++ app/tests/conftest.py
@@ -0,0 +1,6 @@
+# tests/conftest.py
+import os
+os.environ.setdefault("CELERY_BROKER_URL", "memory://")
+os.environ.setdefault("CELERY_RESULT_BACKEND", "cache+memory://")
+os.environ.setdefault("CELERY_TASK_ALWAYS_EAGER", "True")
+os.environ.setdefault("CELERY_TASK_EAGER_PROPAGATES_EXCEPTIONS", "True")
